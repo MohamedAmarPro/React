999ef134bf96d16dcf18b5c4a5e6c7e02d7cc6b7
AitAmar Mohamed <muhamed.aitamar@gmail.com>
2023-03-28 12:29:17 +0200

    fix condition

--- my-new-app--mohamed/src/components/PokemonCard.jsx
+++ my-new-app--mohamed/src/components/PokemonCard.jsx
@@ -1,9 +1,28 @@
 
 function PokemonCard() {
-  return (
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+  let pokemon = pokemonList[0]
+
+  // function displayPoke(pokemon) {
+  //   if (pokemon.imgSrc) {
+  //     return <img src={pokemon.imgSrc} alt="" />
+  // } else {
+  //   return <p>???</p>
+  // } }
+
+    return (
     <div>
-      <figure>
-        <img className="poke" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="" />
+      <figure className="">
+        { pokemon.imgSrc ? <img src={pokemon.imgSrc}/> : <p> ??? </p>}
       </figure>
     </div>
   )
