88811579ba16e72a77b59968b912d233ecd20e96
AitAmar Mohamed <muhamed.aitamar@gmail.com>
2023-03-28 14:57:03 +0200

    done props

--- my-new-app--mohamed/src/App.jsx
+++ my-new-app--mohamed/src/App.jsx
@@ -7,9 +7,21 @@ import  style  from "./App.css";
 
 
 function App() {
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ]
+  let pokemon = pokemonList[0];
+  console.log(pokemon);
   return(
     <>
-      <PokemonCard/>
+      <PokemonCard data={pokemon}/>
     </>
   );
 }
